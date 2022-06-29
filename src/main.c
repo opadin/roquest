302a5807a8ced8e0fe8704f25f1116b9ca4220df
Tristan Herwarth <info@cylogic.de>
2022-06-29 18:06:34 +0200

    the @-char is moving ... ;)

--- src/main.c
+++ src/main.c
@@ -1,48 +1,173 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
-const int WIDTH = 800, HEIGHT = 600;
+// TODO tile-size should be variable
+const int TILE_WIDTH = 8;
+const int TILE_HEIGHT = 16;
 
-int main(int argc, char *argv[])
+const int WINDOW_WIDTH = TILE_WIDTH * 100;
+const int WINDOW_HEIGHT = TILE_HEIGHT * 50;
+
+struct global {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Texture* font;
+    int map_width;
+    int map_height;
+    int player_x;
+    int player_y;
+};
+
+struct global g;
+
+_Noreturn void fatal(SDL_PRINTF_FORMAT_STRING const char* format, ...)
 {
-    SDL_Window *window;
-    SDL_Renderer *renderer;
+    va_list argList;
+    va_start(argList, format);
+    char buffer[1024];
+    SDL_vsnprintf(buffer, sizeof(buffer), format, argList);
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Fatal Error", buffer, 0);
+    SDL_Quit();
+    exit(1);
+}
 
-    SDL_SetHint(SDL_HINT_VIDEO_HIGHDPI_DISABLED, "1");
+void* load_file(const char* path, uint32_t* size)
+{
+    SDL_assert(path && path[0]);
 
-    if (SDL_Init(SDL_INIT_TIMER | SDL_INIT_AUDIO | SDL_INIT_EVENTS | SDL_INIT_VIDEO) != 0)
-    {
-        printf("SDL_Init failed: %s\n", SDL_GetError());
-        return 1;
-    }
+    SDL_RWops* rw = SDL_RWFromFile(path, "rb");
+    if (!rw) fatal("could not open file '%s': %s", path, SDL_GetError());
 
-    window = SDL_CreateWindow("roquest", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+    // only max size of 4gb
+    Sint64 tsize = SDL_RWsize(rw);
+    if (tsize < 0 || tsize > SDL_MAX_UINT32) fatal("invalid file size (%I64d)", tsize);
+    uint32_t fsize = (uint32_t)tsize;
 
-    if (window == NULL)
-    {
-        printf("Could not create window: %s\n", SDL_GetError());
-        return 1;
-    }
+    unsigned char* data = malloc(fsize + 1);
+    size_t read = SDL_RWread(rw, data, 1, fsize);
+    if (read != fsize) fatal("could not read entire file '%s'", path);
 
-    renderer = SDL_CreateRenderer(window, -1, 0);
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderClear(renderer);
+    data[fsize] = '\0';
+    SDL_RWclose(rw);
 
-    SDL_RenderPresent(renderer);
+    if (size)
+        *size = fsize;
 
-    SDL_Event event;
-    while (1)
-    {
-        if (SDL_PollEvent(&event))
-        {
-            if (event.type == SDL_QUIT)
-            {
-                break;
-            }
+    return data;
+}
+
+SDL_Texture* load_image(const char* file, int* w, int* h)
+{
+    SDL_assert(file && file[0] && w && h);
+
+    char path[_MAX_PATH + 1];
+    SDL_snprintf(path, sizeof(path), "%s%s\\%s", SDL_GetBasePath(), "res", file);
+
+    uint32_t fsize;
+    void* data = load_file(path, &fsize);
+    int comp;
+    uint8_t* image = stbi_load_from_memory(data, fsize, w, h, &comp, 4);
+    if (!image) fatal("could not load image '%s'", file);
+    free(data);
+
+    SDL_assert(w > 0 && *w % 16 == 0 && *h > 0 && *h % 16 == 0);
+
+    SDL_Surface* surface = SDL_CreateRGBSurfaceWithFormatFrom(image, *w, *h, 32, *w * 4, SDL_PIXELFORMAT_RGBA32);
+    if (!surface) fatal("could not create sdl surface from image '%s'", file);
+
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(g.renderer, surface);
+    if (!texture) fatal("could not create texture from image '%s'", file);
+
+    return texture;
+}
+
+void render_tile(int x, int y, char ch, uint8_t red, uint8_t blue, uint8_t green) {
+    int srcx = (ch % 16) * TILE_WIDTH;
+    int srcy = (ch / 16) * TILE_HEIGHT;
+    int dstx = x * TILE_WIDTH;
+    int dsty = y * TILE_HEIGHT;
+
+    SDL_SetTextureColorMod(g.font, red, green, blue);
+    SDL_RenderCopy(g.renderer, g.font, &(SDL_Rect) { srcx, srcy, TILE_WIDTH, TILE_HEIGHT}, & (SDL_Rect) { dstx, dsty, TILE_WIDTH, TILE_HEIGHT });
+}
+
+void render() {
+
+    int sx = (WINDOW_WIDTH / TILE_WIDTH - g.map_width) / 2;
+    int sy = (WINDOW_HEIGHT / TILE_HEIGHT - g.map_height) / 2;
+
+    // simple blocked map
+    for (int y = 0; y < g.map_width; y++) {
+        for (int x = 0; x < g.map_height; x++) {
+            render_tile(sx + x, sy + y, '.', 63, 63, 63);
         }
     }
 
-    SDL_DestroyWindow(window);
+    // player
+    render_tile(sx + g.player_x, sy + g.player_y, '@', 255, 255, 255);
+}
+
+int main(int argc, char* argv[])
+{
+    SDL_SetHint(SDL_HINT_VIDEO_HIGHDPI_DISABLED, "1");
+
+    if (SDL_Init(SDL_INIT_TIMER | SDL_INIT_AUDIO | SDL_INIT_EVENTS | SDL_INIT_VIDEO) != 0)
+        fatal("SDL_Init failed: %s\n", SDL_GetError());
+
+    g.window = SDL_CreateWindow("roquest", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!g.window) fatal("Could not create window: %s\n", SDL_GetError());
+
+    g.renderer = SDL_CreateRenderer(g.window, -1, 0);
+    if (!g.renderer) fatal("could not create sdl renderer: %s", SDL_GetError());
+
+    SDL_SetRenderDrawColor(g.renderer, 255, 0, 0, 255);
+    SDL_RenderClear(g.renderer);
+
+    int fw, fh;
+    g.font = load_image("default-font.png", &fw, &fh);
+    SDL_assert(fw == TILE_WIDTH * 16 && fh == TILE_HEIGHT * 16);
+
+    g.map_width = g.map_height = 30;
+    g.player_x = g.map_width / 2;
+    g.player_y = g.map_height / 2;
+
+    SDL_Event event;
+    bool quit_requested = false;
+    while (!quit_requested)
+    {
+        if (SDL_PollEvent(&event))
+        {
+            switch (event.type) {
+                case SDL_QUIT:
+                    quit_requested = true;
+                    break;
+                case SDL_KEYDOWN:
+                    switch (event.key.keysym.scancode) {
+                        case SDL_SCANCODE_UP:
+                            if (g.player_y > 0) g.player_y--;
+                            break;
+                        case SDL_SCANCODE_DOWN:
+                            if (g.player_y < g.map_height - 1) g.player_y++;
+                            break;
+                        case SDL_SCANCODE_LEFT:
+                            if (g.player_x > 0) g.player_x--;
+                            break;
+                        case SDL_SCANCODE_RIGHT:
+                            if (g.player_x < g.map_width - 1) g.player_x++;
+                        default:
+                            break;
+                    }
+            }
+        }
+
+        render();
+        SDL_RenderPresent(g.renderer);
+    }
+
+    SDL_DestroyWindow(g.window);
 
     SDL_Quit();
     return 0;
